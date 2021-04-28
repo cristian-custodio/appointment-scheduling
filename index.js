48d6054e7c7599b50f93535a57d12dfaac90cff1
Cristian Custodio <cristian325@gmail.com>
2021-04-28 16:43:49 -0400

    Moved react.js files to client folder and added node.js server and dependencies.

---
+++ index.js
@@ -0,0 +1,22 @@
+const winston = require("winston");
+const express = require("express");
+const config = require("config");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+module.exports = server;
