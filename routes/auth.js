226f9e9f964a567832edc9a17a020dbc219dc068
Cristian Custodio <cristian325@gmail.com>
2021-04-29 14:41:14 -0400

    User login now working

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,34 @@
 const Joi = require("joi");
+const config = require("config");
 // const bcrypt = require("bcrypt");
 const _ = require("lodash");
 // const { User } = require("../models/user");
 const express = require("express");
+let firebase = require("firebase-admin");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
 
+  //Validate Body
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Get values from body
+    let user = _.pick(req.body, ["email", "password"]);
+
+    user = await firebase.auth().getUserByEmail(user.email);
+
+     //Create JSON Web Token
+  const token = jwt.sign(
+    {
+      _id: user.uid,
+      email: user.email,
+    },
+    config.get("jwtPrivateKey")
+  );
+    
+  
   res.send(token);
 });
 
